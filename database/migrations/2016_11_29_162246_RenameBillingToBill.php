8bce15fbc66e5d029673f5485e48371c2bf3b45d
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-11-29 17:29:24 +0100

    Rename table Billing & add payment_id

--- database/migrations/2016_11_29_162246_RenameBillingToBill.php
+++ database/migrations/2016_11_29_162246_RenameBillingToBill.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPaymentId extends Migration
+class RenameBillingToBill extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,7 @@ class AddPaymentId extends Migration
      */
     public function up()
     {
-        Schema::table('billing', function (Blueprint $table) {
-            $table->string('payment_id')->after('id');
-        });
+        Schema::rename('billing', 'bill');
     }
 
     /**
@@ -25,8 +23,6 @@ class AddPaymentId extends Migration
      */
     public function down()
     {
-        Schema::table('billing', function (Blueprint $table) {
-            $table->dropColumn('payment_id');
-        });
+        Schema::rename('bill', 'billing');
     }
 }
