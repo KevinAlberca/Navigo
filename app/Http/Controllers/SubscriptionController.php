f884a9c7e8d293d03fb9d66c5a9bc1762a56fba9
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-12-07 23:20:57 +0100

    [FEAT] User can renew his card

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -15,8 +15,9 @@ class SubscriptionController extends Controller
         ]);
     }
 
-    public function registerUserSubscription($payment_id = 1, $plan_id = 2, $card_id = null) {
-        return DB::table('bill')->insert([
+    public function registerUserSubscription($payment_id, $plan_id, $card_id = null) {
+    	$card_id != null ? $this->_updateCard($card_id, $plan_id) : null;
+    	$res = DB::table('bill')->insert([
             'payment_id' => $payment_id,
             'users_id' => Auth::user()->id,
             'cards_id' => $card_id == null ? $this->_createCard(Auth::user()->id, $plan_id) : $card_id,
@@ -44,11 +45,37 @@ class SubscriptionController extends Controller
         DB::table('cards')->insert([
             'id' => $card_id,
             'users_id' => $user_id,
-            'subscription_start' => $today->format('Y-m-d H:i:s'),
-            'subscription_end' => $end->format('Y-m-d H:i:s'),
+            'subscription_start' => $today->format('Y-m-d'),
+            'subscription_end' => $end->format('Y-m-d'),
             'is_active' => true,
         ]);
 
         return $card_id;
     }
+
+    private function _updateCard($card_id, $plan_id) {
+        $card = DB::table('cards')->where('id', '=', $card_id)->get();
+        $plans = DB::table('plans')->where('id', '=', $plan_id)->get();
+
+        $today = new \DateTime();
+        if($today->format('Y-m-d') > $card[0]->subscription_end) {
+            $end = new \DateTime();
+            $end->modify('+1 '.$plans[0]->duration);
+        } else {
+            $end = \DateTime::createFromFormat('Y-m-d', $card[0]->subscription_end);
+            $end->modify('+1 '.$plans[0]->duration);
+        }
+       
+        
+        $res = DB::table('cards')
+            ->where('id', '=', $card_id)
+            ->update([
+                'subscription_start' => $today->format('Y-m-d'),
+                'subscription_end' => $end->format('Y-m-d'),
+                'is_active' => true,
+            ]);
+
+        return $res;
+    }
+
 }
