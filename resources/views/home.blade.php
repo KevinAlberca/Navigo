0e4bf1e2258a85f3b87c7582bc5675b141d85c61
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-12-16 00:58:52 +0100

    Display card in color if is_active 0/1?

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,26 @@
     <div class="panel panel-default">
         <div class="panel-heading">Dashboard</div>
         <div class="panel-body">
+
             <h2 class="text-center">{{ ucwords(trans('cards.your_cards')) }}</h2>
-            @if($card)
+            @if(count($card) == 0)
                 {{ ucfirst(trans('cards.empty_cards')) }} <a href="{{ url('/subscription') }}">{{ trans('cards.subscribe') }}</a> ?
             @else
                 @foreach($card as $c)
-                    Subscribed at : {{ $c->subscription_start }}<br/>
-                    End of the subscription : {{ $c->subscription_end }}<br/>
-                    IS active ? {{ $c->is_active }}<br/>
-                    <a href="/renew/{{ Security::encrypt('encrypt', $c->id) }}" class="btn btn-primary">Renew</a>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="panel {{ $c->is_active ? 'panel-green' : 'panel-danger' }}">
+                            <div class="panel-heading">
+                                <h3 class="panel-title text-center">{{ $c->id }}</h3>
+                            </div>
+                            <div class="panel-body">
+                                <ul class="list-unstyled">
+                                    <li>Subscribed at : {{ $c->subscription_start }}</li>
+                                    <li>End of the subscription : {{ $c->subscription_end }}</li>
+                                    <li class="text-center"><a href="/renew/{{ Security::encrypt('encrypt', $c->id) }}" class="btn btn-primary">Renew</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             @endif
         </div>
