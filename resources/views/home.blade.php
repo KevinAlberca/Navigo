2d6596f85d1599b1e84088be7ecac33c77f46fea
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-12-16 00:53:08 +0100

    Remake homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,30 +8,17 @@
     <div class="panel panel-default">
         <div class="panel-heading">Dashboard</div>
         <div class="panel-body">
-            <!-- Nav tabs -->
-            <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-                <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-            </ul>
-
-            <!-- Tab panes -->
-            <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="home">
-                    @foreach($card as $c)
-                        Subscribed at : {{ $c->subscription_start }}<br/>
-                        End of the subscription : {{ $c->subscription_end }}<br/>
-                        IS active ? {{ $c->is_active }}<br/>
-                        <a href="/renew/{{ Security::encrypt('encrypt', $c->id) }}" class="btn btn-primary">Renew</a>
-                    @endforeach
-                </div>
-                <div role="tabpanel" class="tab-pane" id="profile">
-                    profile
-                </div>
-                <div role="tabpanel" class="tab-pane" id="messages">
-                    messages
-                </div>
-            </div>
+            <h2 class="text-center">{{ ucwords(trans('cards.your_cards')) }}</h2>
+            @if($card)
+                {{ ucfirst(trans('cards.empty_cards')) }} <a href="{{ url('/subscription') }}">{{ trans('cards.subscribe') }}</a> ?
+            @else
+                @foreach($card as $c)
+                    Subscribed at : {{ $c->subscription_start }}<br/>
+                    End of the subscription : {{ $c->subscription_end }}<br/>
+                    IS active ? {{ $c->is_active }}<br/>
+                    <a href="/renew/{{ Security::encrypt('encrypt', $c->id) }}" class="btn btn-primary">Renew</a>
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
